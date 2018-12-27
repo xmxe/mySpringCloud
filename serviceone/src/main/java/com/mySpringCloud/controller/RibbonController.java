e582749530d7b2013a2ae52378ecc01785bc196d
xmxe <464817304@qq.com>
2018-12-27 15:52:36 +0800

    serviceone错误修正

--- serviceone/src/main/java/com/mySpringCloud/controller/RibbonController.java
+++ serviceone/src/main/java/com/mySpringCloud/controller/RibbonController.java
@@ -1,4 +1,4 @@
-package com.service;
+package com.mySpringCloud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +10,12 @@ public class RibbonController {
     @Autowired
     RestTemplate restTemplate;
 
+    @RequestMapping("/hello")
+	public String hello(String name){return "hello,这是由serviceone发布的--- "+name;}
+
+	//访问此接口调用其他注册的微服务
     @RequestMapping("/sone")
     public String hi(@RequestParam String name){
         return restTemplate.getForObject("http://servicetwo/hi?name="+name, String.class);
     }
-
-    @RequestMapping("/hello")
-	public String hi(String name){return "hello,这是serviceone调用的--- "+name;}
 }
