2cdbaebaa579e6bd88bcbb824f6a422eb5b6c486
xmxe <464817304@qq.com>
2018-12-28 20:00:32 +0800

    修改版本

--- servicetwo/src/test/java/com/mySpringCloud/ServiceTwoApplicationTests.java
+++ servicetwo/src/test/java/com/mySpringCloud/ServiceTwoApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ServiceOneApplicationTests {
+public class ServiceTwoApplicationTests {
 
 	@Test
 	public void contextLoads() {
