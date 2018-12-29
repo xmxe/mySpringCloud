991653538fbe7a946b272cf99f90658cd1402f90
xmxe <464817304@qq.com>
2018-12-29 08:54:09 +0800

    spring cloud config

--- serviceone/src/main/java/com/mySpringCloud/ServiceOneApplication.java
+++ serviceone/src/main/java/com/mySpringCloud/ServiceOneApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
+@EnableConfigServer
 @SpringBootApplication
 public class ServiceOneApplication {
 
--- servicetwo/src/main/java/com/mySpringCloud/controller/FeignClientController.java
+++ servicetwo/src/main/java/com/mySpringCloud/controller/FeignClientController.java
@@ -2,12 +2,15 @@ package com.mySpringCloud.controller;
 
 import com.mySpringCloud.service.FeignClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FeignClientController {
+    @Value("${jdbcUrl}")
+    String jdbcUrl;
 
     @Autowired
     FeignClientService feignClient;
@@ -19,6 +22,14 @@ public class FeignClientController {
     @GetMapping(value = "stwo")
     public String ttt(String name){
         return feignClient.serviceOne(name);
+    }
+
+    //从spring cloud config(git)上获取得到的
+
+
+    @RequestMapping("/getJdbcUrl")
+    public String getJdbcUrl() {
+        return "jdbcUrl:" + jdbcUrl;
     }
 
 }
