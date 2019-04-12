128a9bb61455d36e7e4447e00b5e43421f3228cc
xmxe <464817304@qq.com>
2019-04-12 13:37:29 +0800

    升级Finchley.RELEASE版本v

--- servicetwo/src/main/java/com/mySpringCloud/service/FeignClientService.java
+++ servicetwo/src/main/java/com/mySpringCloud/service/FeignClientService.java
@@ -1,10 +1,13 @@
 package com.mySpringCloud.service;
 
-import org.springframework.cloud.netflix.feign.FeignClient;
+import com.mySpringCloud.hystrix.ProducerRemoteHystrix;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-@FeignClient(value = "serviceone")//对应serviceone服务
+@Component
+@FeignClient(value = "serviceone",fallback = ProducerRemoteHystrix.class)//对应serviceone服务
 public interface FeignClientService {
     //对应serviceone服务中的hello接口
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
