c40b124e7af743793db2d0a9eeb18ab47c693c0a
xmxe <464817304@qq.com>
2019-07-11 10:51:43 +0800

    fix

--- serviceone/src/main/java/com/ribbon/controller/RibbonController.java
+++ serviceone/src/main/java/com/ribbon/controller/RibbonController.java
@@ -15,6 +15,7 @@ import javax.servlet.http.HttpSession;
 public class RibbonController {
     @Autowired
     RestTemplate restTemplate;
+
     //@Autowired
     //HttpServletRequest request;
 
--- servicetwo/src/main/java/com/feign/controller/FeignClientController.java
+++ servicetwo/src/main/java/com/feign/controller/FeignClientController.java
@@ -2,6 +2,7 @@ package com.feign.controller;
 
 import com.feign.service.FeignClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RefreshScope//在客户端执行/refresh的时候就会更新此类下面的变量值
 @Controller
 public class FeignClientController {
-    //@Value("${jdbcUrl}")
+    @Value("${jdbcUrl}")
     String jdbcUrl;
 
     @Autowired
@@ -22,7 +23,7 @@ public class FeignClientController {
     public String hi(String name){return "hi,这是由servicetwo提供的，会由ribbon调用-- "+name;}
 
     //调用其他注册的微服务
-    @GetMapping(value = "sresis")
+    @GetMapping(value = "sredis")
     @ResponseBody
     public String sredis(){
         return feignClient.session_redis();
