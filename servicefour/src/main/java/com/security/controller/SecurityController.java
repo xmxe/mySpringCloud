9446007ca87742000399cc3753cc7ce2951a88f4
xmxe <464817304@qq.com>
2019-07-11 10:29:49 +0800

    session共享

--- servicefour/src/main/java/com/security/controller/SecurityController.java
+++ servicefour/src/main/java/com/security/controller/SecurityController.java
@@ -1,9 +1,13 @@
 package com.security.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 //@RestController //注意：要跳转到html页面@controller很重要，不要使用@restcontroller，因为他是返回json
 @Controller
@@ -66,4 +70,16 @@ public class SecurityController {
         return "permission4Data";
     }
 
+    @RequestMapping("/session_redis")
+    @ResponseBody
+    public String redis_session(HttpServletRequest request){
+        HttpSession session = request.getSession();
+        String name = (String) session.getAttribute("name");
+        if(StringUtils.isEmpty(name)){
+            name = "testSessionRedis|" + System.currentTimeMillis();
+            session.setAttribute("name", name);
+        }
+        System.out.println("访问端口：" + request.getServerPort());
+        return name;
+    }
 }
--- serviceone/src/main/java/com/ribbon/controller/RibbonController.java
+++ serviceone/src/main/java/com/ribbon/controller/RibbonController.java
@@ -2,14 +2,21 @@ package com.ribbon.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class RibbonController {
     @Autowired
     RestTemplate restTemplate;
+    //@Autowired
+    //HttpServletRequest request;
 
     @RequestMapping("/hello")
     @ResponseBody
@@ -29,5 +36,20 @@ public class RibbonController {
     @RequestMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @RequestMapping("/session_redis")
+    @ResponseBody
+    public String redis_session(HttpServletRequest request){
+        //HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        HttpSession session = request.getSession();
+        String name = (String) session.getAttribute("name");
+        if(StringUtils.isEmpty(name)){
+            name = "负载均衡testSessionRedis|" + System.currentTimeMillis();
+            session.setAttribute("name", name);
+        }
+        System.out.println("访问端口：" + request.getServerPort());
+        System.out.println(request.getParameter("name"));
+        return name;
     }
 }
