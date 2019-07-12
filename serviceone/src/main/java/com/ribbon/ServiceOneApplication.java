174b505e787dd423e648e3d6e765a5439e79f5ca
xmxe <464817304@qq.com>
2019-07-12 09:15:45 +0800

    暂时注释redis-session相关功能

--- serviceone/src/main/java/com/ribbon/ServiceOneApplication.java
+++ serviceone/src/main/java/com/ribbon/ServiceOneApplication.java
@@ -2,13 +2,14 @@ package com.ribbon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient //适用于其他的注册中心，场景较丰富
+@EnableEurekaClient//适用于eureka作为注册中心 场景较单一
 @EnableConfigServer
 @SpringBootApplication
 public class ServiceOneApplication {
