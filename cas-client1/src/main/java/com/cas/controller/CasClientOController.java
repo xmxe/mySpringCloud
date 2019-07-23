080780ebcc366cc2e6442f85061bff64e5bb505f
xmxe <464817304@qq.com>
2019-07-23 11:51:15 +0800

    登出

--- cas-client1/src/main/java/com/cas/controller/CasClientOController.java
+++ cas-client1/src/main/java/com/cas/controller/CasClientOController.java
@@ -1,13 +1,15 @@
 package com.cas.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
 
-@RestController
+@Controller
 public class CasClientOController {
     @RequestMapping("/c1")
+    @ResponseBody
     public String c1(){
         return "success-c1";
     }
@@ -15,7 +17,11 @@ public class CasClientOController {
     @RequestMapping("/logout")
     public String logout(HttpSession session) {
         session.invalidate();
-        return "redirect:http://192.168.236.131:8080/cas/logout?service=http://127.0.0.1:9100/";
+        return "redirect:http://192.168.236.131:8080/cas/logout?service=http://127.0.0.1:9100/index";
     }
-
+    @RequestMapping("/index")
+    @ResponseBody
+    public String index(){
+        return "退出成功";
+    }
 }
