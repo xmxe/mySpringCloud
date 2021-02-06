4b773a0689530230ce1543c3122055038b8f3268
xmxe <464817304@qq.com>
2021-02-06 21:00:14 +0800

    spring cloud gateway自定义过滤器(GatewayFilter)

--- springcloud-gateway/src/main/java/com/xmxe/config/GateWayConfig.java
+++ springcloud-gateway/src/main/java/com/xmxe/config/GateWayConfig.java
@@ -3,13 +3,31 @@ package com.xmxe.config;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import reactor.core.publisher.Mono;
 
-//@Configuration //限流 通过redis的key来实现
+//@Configuration
 public class GateWayConfig {
+    /*
+     * 限流 通过redis的key来实现
+     */
     //@Bean
     public KeyResolver ipKeyResolver() {//设置根据请求 IP 地址来限流
+       /* return new KeyResolver() {
+            @Override
+            public Mono<String> resolve(ServerWebExchange exchange) {
+                return Mono.just(exchange.getRequest().getRemoteAddress().getHostName());
+            }
+        };*/
+
+        // 根据主机名限流
         return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getHostName());
+        // 根据远程地址ip限流
         //return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
-        //return exchange -> Mono.just(exchange.getRequest().getQueryParams().getFirst("user"));//根据请求参数中的 user 字段来限流
-        //return exchange -> Mono.just(exchange.getRequest().getURI().getPath());//根据uri限流
+
+        // 根据请求参数中的 user 字段来限流
+        // return exchange -> Mono.just(exchange.getRequest().getQueryParams().getFirst("user"));
+
+        // 根据uri限流
+        // return exchange -> Mono.just(exchange.getRequest().getURI().getPath());
     }
+
+
 }
